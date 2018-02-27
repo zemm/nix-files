d139b239ceac8f1a3d1da973def7318d32de653e
Jussi Rajala <zemm@iki.fi>
2018-02-27 19:43:59 +0200

    Initial files from work

---
+++ common.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  boot.cleanTmpDir = true;
+
+  environment.shellAliases.homekeeper =
+    "git --git-dir=\"\${HOME}/.homekeeper\" --work-tree=\"\${HOME}\"";
+
+  #nixpkgs.overlays = [
+  #  (import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/overlay.nix")
+  #];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    elinks
+    gitFull
+    nano
+    tmux
+    vim
+    wget
+    #home-manager
+  ];
+
+  fonts.enableFontDir = true;
+  fonts.enableCoreFonts = true;
+  fonts.enableGhostscriptFonts = true;
+  fonts.fonts = with pkgs; [
+    bakoma_ttf
+    cantarell_fonts
+    corefonts
+    dejavu_fonts
+    gentium
+    inconsolata
+    liberation_ttf
+    powerline-fonts
+    source-code-pro
+    terminus_font
+    ubuntu_font_family
+  ];
+
+  i18n.consoleFont = "Lat2-Terminus16";
+  i18n.consoleKeyMap = "colemak/en-latin9";
+  i18n.defaultLocale = "fi_FI.UTF-8";
+
+  nixpkgs.config.allowUnfree = true;
+
+  time.timeZone = "Europe/Helsinki";
+
+  services.xserver.layout = "us";
+  services.xserver.xkbVariant = "colemak";
+  services.xserver.xkbOptions = "eurosign:e,caps:backspace,nbsp:none";
+
+}
---
+++ hardware-5520.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  # hardware.bluetooth.enable = true;
+
+  hardware.bumblebee = {
+    enable = true;
+    connectDisplay = true;
+  };
+
+  services.xserver.libinput = {
+    enable = true;
+    accelProfile = "adaptive";
+    accelSpeed = "0.7";
+    scrollMethod = "twofinger";
+    tapping = false;
+    tappingDragLock = false;
+    naturalScrolling = true;
+    disableWhileTyping = true;
+    additionalOptions = ''
+      Option "SendEventsMode" "disabled-on-external-mouse"
+    '';
+  };
+
+  services.xserver.videoDrivers = [
+  #  "nvidia"
+    "intel"
+  ];
+}
